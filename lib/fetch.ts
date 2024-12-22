064c1d963dc7517d3983cac62beb6a5cdefc9034
hiroki1953 <hirokisoccer0709@gmail.com>
2024-12-22 16:19:02 +0900

    エンドポイント修正

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,11 +1,10 @@
-// utils/fetch.ts
 export async function fetchFromApi(endpoint: string, options?: RequestInit) {
   const baseUrl =
-    "https://my-cooking-9p60aapqg-hiroki1953s-projects.vercel.app/" ||
-    "http://localhost:3000";
+    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
   const fullUrl = `${baseUrl}${endpoint}`;
 
   const res = await fetch(fullUrl, options);
+
   if (!res.ok) {
     throw new Error(
       `API call failed with status ${res.status}: ${res.statusText}`
